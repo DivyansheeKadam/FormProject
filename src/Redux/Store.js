ef54bb42a701b9ed7daa912a295045cb4e56f7a3
Divyanshee Kadam <103111723+divykadam@users.noreply.github.com>
2023-04-13 17:48:11 +0530

    Delete src directory

--- src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import Reducer from '../Reducer/Reducer';
-const rootReducer = combineReducers({
-  reducer:Reducer})
-  const store=createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  // console.warn(store.getState())
-
-  export default store;
